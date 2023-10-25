26aa65a0332a0d9ab20b8379bb57dc20ac3217e2
Yousef-FJ <47510107+Yousif-FJ@users.noreply.github.com>
2023-10-25 03:10:30 +0300

    Add project files.

---
+++ RequirementGeneratorForOpenReq/Program.cs
@@ -0,0 +1,62 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using RequirementGeneratorForOpenReq;
+using System.Globalization;
+using System.Text.Json;
+
+var currentDirectory = Directory.GetCurrentDirectory();
+
+
+var inputFilePath = Directory.GetFiles(currentDirectory)
+    .Where(file => file.EndsWith(".csv"))
+    .FirstOrDefault();
+
+if (inputFilePath is null)
+{
+    PrintMessageAndWaitForInput("Can not file csv file in the current directory");
+    return;
+}
+
+try
+{
+    using var reader = new StreamReader(inputFilePath);
+
+    var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+    {
+        HasHeaderRecord = false,
+    };
+
+    using var csv = new CsvReader(reader, config);
+
+    var records = csv.GetRecords<RequirementRecord>();
+    var openReqRoot = new RequirementsOpenReqRoot()
+    {
+        Requirements = records,
+    };
+
+
+    var jsonOption = new JsonSerializerOptions()
+    {
+        WriteIndented = true,
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+    };
+    var jsonOpenReqRoot = JsonSerializer.Serialize(openReqRoot, jsonOption);
+
+    var resultFilePath = Path.Combine(currentDirectory,$"{Path.GetFileNameWithoutExtension(inputFilePath)}.json");
+    File.WriteAllText(resultFilePath, jsonOpenReqRoot);
+
+    PrintMessageAndWaitForInput("Done..");
+
+}
+catch(Exception e)
+{
+    PrintMessageAndWaitForInput($"Exception occured: {e.Message}");
+    return;
+}
+
+
+static void PrintMessageAndWaitForInput(string EexitMessage)
+{
+    Console.WriteLine(EexitMessage);
+    Console.ReadLine();
+}
---
+++ RequirementGeneratorForOpenReq/RequirementRecord.cs
@@ -0,0 +1,11 @@
+﻿namespace RequirementGeneratorForOpenReq;
+public class RequirementRecord
+{
+    public string? Id { get; set; }
+    public string? Text { get; set; }
+}
+
+public class RequirementsOpenReqRoot
+{
+    public IEnumerable<RequirementRecord> Requirements { get; set; } = new List<RequirementRecord>();
+}
