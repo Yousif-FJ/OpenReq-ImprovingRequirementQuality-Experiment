89284a030f55d8c95a72be4f4703671f4c24e011
Yousif FJ <47510107+Yousif-FJ@users.noreply.github.com>
2023-11-14 14:08:20 +0200

    Add files

---
+++ OpenReq/CustomTools/RequirementGeneratorForOpenReq/CommonTools/RequirementRecord.cs
@@ -0,0 +1,11 @@
+﻿namespace CommonTools;
+public class RequirementRecord
+{
+    public string? Id { get; set; }
+    public string? Text { get; set; }
+}
+
+public class RequirementsOpenReqRoot
+{
+    public IList<RequirementRecord> Requirements { get; set; } = new List<RequirementRecord>();
+}
---
+++ OpenReq/CustomTools/RequirementGeneratorForOpenReq/TextFileRunner/Program.cs
@@ -0,0 +1,56 @@
+﻿using CommonTools;
+using CsvHelper;
+using Newtonsoft.Json;
+using System.Globalization;
+using System.Net.Http.Json;
+
+var currentDirectory = Directory.GetCurrentDirectory();
+
+var inputDirectory = Path.Combine(currentDirectory, "sample");
+
+var intermediateDirectory = Path.Combine(currentDirectory, "intermediate");
+Directory.CreateDirectory(intermediateDirectory);
+
+
+var outputDirectory = Path.Combine(currentDirectory, "output");
+Directory.CreateDirectory(outputDirectory);
+
+var inputFilePaths = Directory.GetFiles(inputDirectory)
+    .Where(file => file.EndsWith(".txt"));
+
+
+foreach (var filePath in inputFilePaths)
+{
+    var requirements = File.ReadAllText(filePath).Split(Environment.NewLine);
+
+    var id = 1;
+    var requestObj = new RequirementsOpenReqRoot();
+
+    foreach (var requirement in requirements)
+    {
+        requestObj.Requirements.Add(new RequirementRecord
+        {
+            Id = id.ToString(),
+            Text = requirement
+        });
+
+        ++id;
+    }
+
+    using var httpClient = new HttpClient();
+    httpClient.BaseAddress = new Uri("http://localhost:9799");
+    var response = await httpClient.PostAsJsonAsync("/check-quality", requestObj);
+
+    var result = await response.Content.ReadAsStringAsync();
+    var json = JsonConvert.DeserializeObject(result);
+    result = JsonConvert.SerializeObject(json, Formatting.Indented);
+
+    var intermediateFilePath = Path.Combine(intermediateDirectory, $"{Path.GetFileNameWithoutExtension(filePath)}.csv");
+    using var writer = new StreamWriter(intermediateFilePath);
+    using var csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
+    csv.WriteRecords(requestObj.Requirements);
+    
+
+    var resultFilePath = Path.Combine(outputDirectory, $"{Path.GetFileNameWithoutExtension(filePath)}.json");
+    File.WriteAllText(resultFilePath, result);
+}
