ef2129f5237fcf5241adc265f26a711bbd4f53e5
Yousif FJ <47510107+Yousif-FJ@users.noreply.github.com>
2023-11-16 01:25:30 +0200

    Update OpenReq script

---
+++ OpenReq/Scripts/RequirementGeneratorForOpenReq/OutputTypeCounter/AmbiguityModel.cs
@@ -0,0 +1,10 @@
+﻿namespace OutputTypeCounter;
+internal class AmbiguityModel
+{
+    public string? Description { get; set; }
+    public int Index_end { get; set; }
+    public int Index_start { get; set; }
+    public string? Language_construct { get; set; }
+    public string? Text { get; set; }
+    public string? Title { get; set; }
+}
---
+++ OpenReq/Scripts/RequirementGeneratorForOpenReq/OutputTypeCounter/Program.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using OutputTypeCounter;
+
+var inputFilePath = Directory.GetFiles(Directory.GetCurrentDirectory())
+    .Where(file => file.EndsWith(".json"))
+    .First();
+
+var inputString = File.ReadAllText(inputFilePath);
+
+
+var IdAmbiguityListKeyValuePair = JsonConvert.DeserializeObject<Dictionary<string, IEnumerable<AmbiguityModel>>>(inputString)!;
+
+var titleCount = new Dictionary<string, int>();
+
+foreach (var ambiguityList in IdAmbiguityListKeyValuePair.Values)
+{
+    foreach (var ambiguity in ambiguityList)
+    {
+        if (ambiguity is null)
+        {
+            continue;
+        }
+
+        if (titleCount.TryGetValue(ambiguity.Title!, out int count))
+        {
+            titleCount[ambiguity.Title!] = count + 1;
+        }
+        else
+        {
+            titleCount[ambiguity.Title!] = 1;
+        }
+    }
+}
+
+Console.WriteLine(JsonConvert.SerializeObject(titleCount, Formatting.Indented));
